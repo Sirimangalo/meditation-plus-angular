b265553f38e9a01f51742613c13017177cf4973c
Martin <bieder.martin@googlemail.com>
2016-07-30 22:02:59 +0200

    feat(home): caches tabs

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -7,10 +7,14 @@ import { AppState } from '../';
 @Component({
   selector: 'home',
   template: require('./home.html'),
+  styles: [
+    require('./home.css')
+  ],
   directives: [ MessageComponent, MeditationComponent, CommitmentComponent ]
 })
 export class Home {
   currentTab: string = 'meditation';
+  activated: string[] = ['meditation'];
 
   constructor(public appState: AppState) {
     this.appState.set('title', '');
@@ -20,7 +24,14 @@ export class Home {
     return this.currentTab === tab ? 'primary' : '';
   }
 
+  isCurrentTab(tab: string): boolean {
+    return this.currentTab === tab;
+  }
+
   tab(tab: string) {
     this.currentTab = tab;
+    if (this.activated.indexOf(tab) < 0) {
+      this.activated.push(tab);
+    }
   }
 }
