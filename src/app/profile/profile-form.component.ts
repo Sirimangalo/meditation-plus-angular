4a661b47f780d094c314fba3c8038f88dbe34bad
Martin <bieder.martin@googlemail.com>
2016-08-10 17:27:42 +0200

    refactor(profile): moves basic form to UserFormComponent for reuse

--- src/app/profile/profile-form.component.ts
+++ src/app/profile/profile-form.component.ts
@@ -3,10 +3,12 @@ import { UserService } from '../user/user.service';
 import { Observable } from 'rxjs/Rx';
 import { AppState } from '../';
 import { Country } from './country';
+import { UserFormComponent } from '../user';
 
 @Component({
   selector: 'profile-form',
   template: require('./profile-form.html'),
+  directives: [UserFormComponent],
   styles: [
     require('./profile-form.css')
   ]
@@ -16,15 +18,6 @@ export class ProfileFormComponent {
   profile;
   loading: boolean = false;
   updated: boolean = false;
-  countryList = Country.list;
-  sounds: Object[] = [
-    { name: 'Bell 1', src: '/assets/audio/bell.mp3'},
-    { name: 'Bell 2', src: '/assets/audio/bell1.mp3'},
-    { name: 'Birds', src: '/assets/audio/birds.mp3'},
-    { name: 'Bowl', src: '/assets/audio/bowl.mp3'},
-    { name: 'Gong', src: '/assets/audio/gong.mp3'}
-  ];
-  currentSound;
 
   constructor(
     public userService: UserService,
@@ -51,32 +44,17 @@ export class ProfileFormComponent {
     this.loading = true;
     this.userService.updateProfile(this.profile)
       .subscribe(
-        () => {
-          this.updated = true;
-        },
+        () => this.updated = true,
         err => console.log(err),
         () => this.loading = false
       );
-  }
-
-  playSound() {
-    if (this.profile.sound){
-      this.currentSound = new Audio(this.profile.sound);
-      this.currentSound.play();
-    }
-  }
-  stopSound() {
-    if (this.currentSound){
-      this.currentSound.pause();
-      this.currentSound.currentTime = 0;
-    }
   }
 
   ngOnInit() {
     this.userService.getProfile()
       .map(res => res.json())
       .subscribe(
-        data => { console.log(data); this.profile = data; },
+        data => this.profile = data,
         err => console.error(err)
       );
   }
---
+++ src/app/user/user-form.component.ts
@@ -0,0 +1,42 @@
+import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { UserService } from '../user/user.service';
+import { Observable } from 'rxjs/Rx';
+import { AppState } from '../';
+import { Country } from '../profile/country';
+
+@Component({
+  selector: 'user-form',
+  template: require('./user-form.component.html'),
+  styles: [
+    require('./user-form.component.css')
+  ]
+})
+export class UserFormComponent {
+
+  @Input() model = {};
+  @Output() modelChange: EventEmitter<any> = new EventEmitter<any>();
+
+  countryList = Country.list;
+  sounds: Object[] = [
+    { name: 'Bell 1', src: '/assets/audio/bell.mp3'},
+    { name: 'Bell 2', src: '/assets/audio/bell1.mp3'},
+    { name: 'Birds', src: '/assets/audio/birds.mp3'},
+    { name: 'Bowl', src: '/assets/audio/bowl.mp3'},
+    { name: 'Gong', src: '/assets/audio/gong.mp3'}
+  ];
+  currentSound;
+
+  playSound() {
+    if (this.model.sound){
+      this.currentSound = new Audio(this.model.sound);
+      this.currentSound.play();
+    }
+  }
+
+  stopSound() {
+    if (this.currentSound){
+      this.currentSound.pause();
+      this.currentSound.currentTime = 0;
+    }
+  }
+}
