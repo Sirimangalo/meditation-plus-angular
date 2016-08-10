77b2fa5f916a59a01a100dd2571a9f957613131e
Martin <bieder.martin@googlemail.com>
2016-08-11 01:51:35 +0200

    feat(meditation): switch to barchart + highlight cur. hour Fixes #79

--- src/app/meditation/meditation.component.ts
+++ src/app/meditation/meditation.component.ts
@@ -40,8 +40,11 @@ export class MeditationComponent {
   sitting: string = '';
 
   // chart details
+  chartSubscribtion: Subscription;
+  chartLastHour: string = '';
   chartData: Array<any> = [];
   chartLabels: String[] = [];
+  chartColors: Array<any> = [];
   chartOptions = {
     animations: true,
     maintainAspectRatio: false,
@@ -50,6 +53,10 @@ export class MeditationComponent {
     scales: {
       yAxes: [{
         display: false
+      }],
+      xAxes: [{
+        // needed to have both series (normal and current hour) in one place
+        stacked: true
       }]
     }
   };
@@ -67,21 +74,58 @@ export class MeditationComponent {
     this.polluteWithLastSession();
 
     let data = {data: [], label: 'Meditation minutes'};
+    let dataCurrentHour = {data: [], label: 'Meditation minutes (current hour)'};
+
+    let colors = [];
     for (let i = 0; i < 24; i++) {
       this.chartLabels.push('' + i);
     }
     this.chartData.push(data);
 
-    // create chart data
-    meditationService
-      .getTimes()
-      .map(res => res.json())
-      .subscribe(res => {
-        data = {data: [], label: 'Meditation minutes'};
-        for (let entry of Object.keys(res)) {
-          data.data.push(res[entry]);
+    // check for hour change every second
+    this.chartSubscribtion = Observable.interval(1000)
+      .subscribe(() => {
+        const currentHour = moment().utc().format('H').toString();
+
+        if (currentHour === this.chartLastHour) {
+          return;
         }
-        this.chartData = [data];
+
+        // create chart data on hour change
+        meditationService
+          .getTimes()
+          .map(res => res.json())
+          .subscribe(res => {
+            this.chartLastHour = currentHour;
+
+            // Two datasets are needed to have different colors for the bars.
+            // The current hour should have other color.
+            data = {data: [], label: 'Meditation minutes'};
+            dataCurrentHour = {data: [], label: 'Meditation minutes (current hour)'};
+
+            // normal color
+            colors.push({
+              backgroundColor: 'rgba(255, 33, 81, 0.4)'
+            });
+
+            // current hour
+            colors.push({
+              backgroundColor: 'rgba(255, 33, 81, 0.9)'
+            });
+
+            for (let entry of Object.keys(res)) {
+              // push current hour times only to currentHour chart series
+              if (entry === currentHour) {
+                dataCurrentHour.data.push(res[entry]);
+                data.data.push(0);
+                continue;
+              }
+              data.data.push(res[entry]);
+              dataCurrentHour.data.push(0);
+            }
+            this.chartData = [data, dataCurrentHour];
+            this.chartColors = colors;
+          });
       });
   }
 
@@ -278,5 +322,6 @@ export class MeditationComponent {
   ngOnDestroy() {
     this.meditationSubscription.unsubscribe();
     this.meditationSocket.unsubscribe();
+    this.chartSubscribtion.unsubscribe();
   }
 }
