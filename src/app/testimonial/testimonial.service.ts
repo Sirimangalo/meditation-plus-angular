26c261345f661c2eec2b576b89cadbd2c51060bb
Sebastian Kussl <sebastian.kussl@yahoo.de>
2016-08-12 19:49:04 +0200

    feat: add testimonial admin page

--- src/app/testimonial/testimonial.service.ts
+++ src/app/testimonial/testimonial.service.ts
@@ -22,11 +22,40 @@ export class TestimonialService {
     );
   }
 
+  public getAllAdmin(): Observable<any> {
+    return this.authHttp.get(
+      ApiConfig.url + '/api/testimonial/admin', {
+      headers: new Headers({
+        'Content-Type': 'application/json'
+      })
+      }
+    );
+  }
 
   public post(testimonial: string, anonymous: boolean): Observable<any> {
     return this.authHttp.post(
       ApiConfig.url + '/api/testimonial',
       JSON.stringify({ text: testimonial, anonymous: anonymous }), {
+      headers: new Headers({
+        'Content-Type': 'application/json'
+      })
+    });
+  }
+
+  public toggleReviewed(testimonialId: string): Observable<any> {
+    return this.authHttp.put(
+      ApiConfig.url + '/api/testimonial/review',
+      JSON.stringify({ id : testimonialId }), {
+        headers: new Headers({
+          'Content-Type': 'application/json'
+        })
+      }
+    );
+  }
+
+  public delete(testimonial) {
+    return this.authHttp.delete(
+      ApiConfig.url + '/api/testimonial/' + testimonial._id, {
       headers: new Headers({
         'Content-Type': 'application/json'
       })
