d82e22c70ca466b6d865de2c2f290ba1e87a800d
Sebastian Kussl <sebastian.kussl@yahoo.de>
2016-08-15 18:36:13 +0200

    fix: correct if statement

--- src/app/help/help.component.ts
+++ src/app/help/help.component.ts
@@ -28,6 +28,6 @@ export class HelpComponent {
   }
 
   showSection(whichSection) {
-    this.currentSection = whichSection === this.currentSection ? whichSection : '';
+    this.currentSection = whichSection === this.currentSection ? '' : whichSection;
   }
 }
