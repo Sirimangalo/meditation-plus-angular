68d0687d3945310d86ae571dd405c0b0a772c5d6
Martin <bieder.martin@googlemail.com>
2016-09-11 13:14:01 +0200

    chore(deploy): changes dev server

--- deploy.sh
+++ deploy.sh
@@ -7,12 +7,12 @@ then
   if [ ! -z "$TRAVIS_TAG" ]
   then
     echo "PROD: Deploying $TRAVIS_TAG to server"
-    folder="meditation-plus"
+    server="meditation.sirimangalo.org"
     version=$TRAVIS_TAG
   elif [ "$TRAVIS_BRANCH" = "master" ]
   then
     echo "TEST: Deploying master to test server"
-    folder="meditation-plus-test"
+    server="meditation-dev.sirimangalo.org"
     version="$TRAVIS_BUILD_NUMBER.0.0"
   else
     exit
@@ -23,6 +23,6 @@ then
   touch dist/assets/version.json
   echo "{ \"version\": \"$TRAVIS_TAG\" }" > dist/assets/version.json
   tar -czf transfer-client.tgz dist
-  scp -o "StrictHostKeyChecking no" transfer-client.tgz jenkins@159.203.6.130:/var/www/$folder
-  ssh -o "StrictHostKeyChecking no" jenkins@159.203.6.130 "cd /var/www/$folder; rm -rf client; mkdir client; tar -xzf transfer-client.tgz -C client --strip 1; rm transfer-client.tgz"
+  scp -o "StrictHostKeyChecking no" transfer-client.tgz jenkins@$server:/var/www/meditation-plus
+  ssh -o "StrictHostKeyChecking no" jenkins@$server "cd /var/www/meditation-plus; rm -rf client; mkdir client; tar -xzf transfer-client.tgz -C client --strip 1; rm transfer-client.tgz"
 fi
