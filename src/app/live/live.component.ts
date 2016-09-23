2f2429f67272276bb7c096744e784c2e82495daf
binarious <binarious@users.noreply.github.com>
2016-09-23 18:21:46 +0200

    feat(livestream): add interval subscription (#305)

--- src/app/live/live.component.ts
+++ src/app/live/live.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { LiveService } from './live.service';
 import { AppState } from '../app.service';
+import { Observable } from 'rxjs/Rx';
 import * as moment from 'moment';
 
 @Component({
@@ -13,12 +14,20 @@ import * as moment from 'moment';
 export class LiveComponent {
 
   liveStream;
+  intervalSubscription;
 
   constructor(
     public appState: AppState,
     public liveService: LiveService
   ) {
     appState.set('title', 'Live Stream');
+
+    this.intervalSubscription = Observable.interval(10000)
+      .switchMap(() => this.liveService.getLiveData())
+      .map(res => res.json())
+      .retry()
+      .subscribe(res => this.liveStream = res);
+
     liveService.getLiveData()
       .map(res => res.json())
       .subscribe(res => this.liveStream = res);
@@ -27,8 +36,12 @@ export class LiveComponent {
   get distance(): string {
     let streamTime = moment('1:00:00 +0000', 'HH:mm:ss Z');
     if (moment() > streamTime) {
-      streamTime.add('day', 1);
+      streamTime.add(1, 'day');
     }
     return streamTime.fromNow();
+  }
+
+  ngOnDestroy() {
+    this.intervalSubscription.unsubscribe();
   }
 }
