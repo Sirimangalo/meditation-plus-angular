6740541c2dddd90e065772ff32d612b1f098b9e2
Sebastian <sebastian.kussl@yahoo.de>
2016-12-06 15:11:36 +0100

    feat: add html grid

--- src/app/wiki/wiki.component.ts
+++ src/app/wiki/wiki.component.ts
@@ -5,4 +5,21 @@ import { Component } from '@angular/core';
   template: require('./wiki.component.html'),
 })
 export class WikiComponent {
+
+  category: string;
+
+  tags;
+  categories: string[];
+
+  /**
+   * Change the currently selected category.
+   * @param {string} name name of the category
+   */
+  changeCategory(name: string) {
+    this.category = name;
+  }
+
+  ngOnInit() {
+
+  }
 }
