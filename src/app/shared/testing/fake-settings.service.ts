67ff026b182b7e9180f4bace6212d4b71330fb4d
Sebastian <sebastian.kussl@yahoo.de>
2017-07-19 14:35:56 +0200

    chore: add tests

--- src/app/shared/testing/fake-settings.service.ts
+++ src/app/shared/testing/fake-settings.service.ts
@@ -2,4 +2,11 @@ import { TestHelper } from '../../../testing/test.helper';
 import { Observable } from 'rxjs/Rx';
 
 export class FakeSettingsService {
+  public get() {
+    return TestHelper.noResponse();
+  }
+
+  public set(property, value) {
+    return TestHelper.noResponse();
+  }
 }
